55e46766fe4c23d5aaa49daefed254b567ccf540
sreecharan-desu <sreecharan309@gmail.com>
2025-12-09 17:24:40 +0530

    complete module 1

--- backend/routes/v1/inventory/inventory.ts
+++ backend/routes/v1/inventory/inventory.ts
@@ -37,7 +37,6 @@ router.post(
       const upload = await prisma.upload.create({
         data: {
           storeId: store.id,
-          // @ts-ignore
           filename: parsed.data.filename,
           status: "PENDING",
           metadata: parsed.data.metadata ?? {},
